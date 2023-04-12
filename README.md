b8b917e67ddf409fefa49d98419c63350e7e72c2
Christopher Ariza <ariza@flexatone.com>
2023-04-12 08:25:02 -0700

    code cleanup

--- README.md
+++ README.md
@@ -5,4 +5,30 @@ arraymap
 The ArrayMap library provides dictionary-like lookup from NumPy array values to their integer positions. The hash table design and C implementation is based on [AutoMap](https://github.com/brandtbucher/automap), with extensive additions for direct support of NumPy arrays.
 
 
+Code: https://github.com/static-frame/arraymap
+
+Packages: https://pypi.org/project/arraymap
+
+
+
+Dependencies
+--------------
+
+ArrayKit requires the following:
+
+- Python >= 3.7
+- NumPy >= 1.18.5
+
+
+
+What is New in ArrayMap
+-------------------------
+
+
+
+
+0.1.0
+-------
+
+Initial release with NumPy integration.
 
--- tasks.py
+++ tasks.py
@@ -8,15 +8,12 @@ run = functools.partial(invoke.Context.run, echo=True, pty=True)
 
 @invoke.task
 def install(context):
-    # type: (invoke.Context) -> None
     run(context, f"{sys.executable} -m pip install --upgrade pip")
     run(context, f"{sys.executable} -m pip install --upgrade -r requirements.txt")
 
 
 @invoke.task()
 def clean(context):
-    # type: (invoke.Context) -> None
-    # run(context, f"{sys.executable} setup.py develop --uninstall")
     run(context, f"{sys.executable} -m pip uninstall --yes arraymap")
 
     for artifact in ("*.egg-info", "*.so", "build", "dist"):
@@ -26,12 +23,9 @@ def clean(context):
 
 @invoke.task(clean)
 def build(context):
-    # type: (invoke.Context) -> None
-    # run(context, f"{sys.executable} setup.py develop")
     run(context, f"{sys.executable} -m pip -v install .")
 
 
 @invoke.task(build)
 def test(context):
-    # type: (invoke.Context) -> None
     run(context, f"{sys.executable} -m pytest -v")
