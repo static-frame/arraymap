2a1d01f46d0d7c49abb39e2a77ba8cfc74666aeb
Christopher Ariza <ariza@flexatone.com>
2024-07-11 14:40:14 -0700

    preliminary pyproject.toml

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import os
 
 AM_VERSION = "0.3.0"
 
-with open("README.rst") as file:
-    LONG_DESCRIPTION = file.read()
+# with open("README.rst") as file:
+#     LONG_DESCRIPTION = file.read()
 
 
 def get_ext_dir(*components: tp.Iterable[str]) -> tp.Sequence[str]:
@@ -29,27 +29,27 @@ extension = setuptools.Extension(
 )
 
 setuptools.setup(
-    name="arraymap",
-    version=AM_VERSION,
-    description="Dictionary-like lookup from NumPy array values to their integer positions",
-    long_description=LONG_DESCRIPTION,
-    python_requires=">=3.9",
-    install_requires=["numpy>=1.19.5"],
-    url="https://github.com/static-frame/arraymap",
-    author="Christopher Ariza, Brandt Bucher",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
+    # name="arraymap",
+    # version=AM_VERSION,
+    # description="Dictionary-like lookup from NumPy array values to their integer positions",
+    # long_description=LONG_DESCRIPTION,
+    # python_requires=">=3.9",
+    # install_requires=["numpy>=1.19.5"],
+    # url="https://github.com/static-frame/arraymap",
+    # author="Christopher Ariza, Brandt Bucher",
+    # license="MIT",
+    # classifiers=[
+    #     "Development Status :: 5 - Production/Stable",
+    #     "Intended Audience :: Developers",
+    #     "Topic :: Software Development",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: MacOS :: MacOS X",
+    #     "Operating System :: Microsoft :: Windows",
+    #     "Operating System :: POSIX",
+    #     "Programming Language :: Python :: 3.9",
+    #     "Programming Language :: Python :: 3.10",
+    #     "Programming Language :: Python :: 3.11",
+    #     "Programming Language :: Python :: 3.12",
+    # ],
     ext_modules=[extension],
 )
